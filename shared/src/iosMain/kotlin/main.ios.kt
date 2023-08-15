382a50600da8052528cdc3b9ea3defcd8dcc1f7c
Alex <alxnophis@pm.me>
2023-08-15 18:26:39 +0200

    chore: check ktlint

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,5 +1,7 @@
 import androidx.compose.ui.window.ComposeUIViewController
 
+// ktlint-disable filename
+
 actual fun getPlatformName(): String = "iOS"
 
 fun MainViewController() = ComposeUIViewController { App() }

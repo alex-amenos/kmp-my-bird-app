d298e0254529e1b61ff9eb14f89a4f84705a7389
Alex <alxnophis@pm.me>
2023-12-06 17:38:18 +0100

    refactor: update my logger

--- composeApp/src/commonMain/kotlin/infrastructure/core/common/MyLogger.kt
+++ composeApp/src/commonMain/kotlin/infrastructure/core/common/MyLogger.kt
@@ -1,15 +1,14 @@
 package infrastructure.core.common
 
 import co.touchlab.kermit.Severity
-import co.touchlab.kermit.SimpleFormatter
 import co.touchlab.kermit.loggerConfigInit
 import co.touchlab.kermit.platformLogWriter
 import co.touchlab.kermit.Logger as KermitLogger
 
 object MyLogger : KermitLogger(
     config = loggerConfigInit(
-        platformLogWriter(SimpleFormatter),
+        platformLogWriter(),
         minSeverity = Severity.Verbose,
     ),
-    tag = "MyAppTag",
+    tag = "MyBirdApp",
 )
